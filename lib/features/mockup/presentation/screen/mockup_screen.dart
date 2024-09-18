1a6828ab16036f3eaa5c8537feef9f974b5b060a
Vilaphong Douangmala <vilaphongdouangmala@Vilaphongs-MacBook-Pro.local>
2024-09-18 11:20:35 +0700

    init mockup module

---
+++ lib/features/mockup/presentation/screen/mockup_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MockupScreen extends StatelessWidget {
+  const MockupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
